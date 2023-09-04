00972ced9e0c07acd23bc4396c15fe280e3dba42
Cocotle <luisdavidcocoyuv@gmail.com>
2023-09-04 10:01:28 -0600

    cv

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+workExperience : Array<any> = [];
 
-@Component({
-  selector: 'app-work-experience',
-  templateUrl: './work-experience.component.html',
-  styleUrls: ['./work-experience.component.css']
-})
-export class WorkExperienceComponent {
+constructor() { }
 
-}
+ngOnInit(): void { }
+  
+let work1 = {
+  fecha : "2018 - 2022",
+  ubicacion : "Orizaba, Ver",
+  puesto : "CEO",
+  empresa : "Waves",
+  logros : [
+    { descripcion : "Dron autonomo" },
+    { descripcion : "Cluster GPU" },
+  ]
+};
+
+let work2  = {
+  fecha : "2018 - 2022",
+  ubicacion : "Orizaba, Ver",
+  puesto : "CEO",
+  empresa : "Waves",
+  logros : [
+    { descripcion : "Dron autonomo" },
+    { descripcion : "Cluster GPU" },
+  ]
+};
+
+this.workExperience.push(work1)
+this.workExperience.push(work2)
