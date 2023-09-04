fb325f2473f5e06c50e4de0a11c40e8d3dedd5a2
Cocotle <luisdavidcocoyuv@gmail.com>
2023-09-04 09:00:12 -0600

    init cv

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+name: string = "fake name";
+titulos: string = "fake name";
+mision: string = "fake name";
+foto: string = "fake name";
+
+}
