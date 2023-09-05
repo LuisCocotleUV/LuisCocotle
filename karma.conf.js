6b79bef0c1be77d237dccf982ab5e5b41388d1d1
LuisCocotleUV <113140993+LuisCocotleUV@users.noreply.github.com>
2023-09-05 13:46:30 -0600

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here.
+        // you can add configuration options for Jasmine here
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
@@ -34,14 +34,14 @@ module.exports = function (config) {
     },
     reporters: ['progress', 'kjhtml'],
     browsers: ['Chrome'],
-    browsers: [`ChromeHeadlessCI`],
-      customLaunchers: {
-        ChromeHeadlessCI: {
-          base: `ChromeHeadless`,
-          flags: [`--no-sandbox`]
-        }
-      },
-    singleRun: false,
+    browsers: ['ChromeHeadlessCI'],
+	  customLaunchers: {
+	    ChromeHeadlessCI : {	  
+	      base: 'ChromeHeadless',
+	      flags: ['--no-sandbox']
+	    }
+    },
+    singleRun: false,	  
     restartOnFileChange: true
   });
 };
