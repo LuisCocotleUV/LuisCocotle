65144ac2dbdd822284975a5f5da43ba1917b1342
Cocotle <luisdavidcocoyuv@gmail.com>
2023-09-05 13:54:04 -0600

    corregir

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
-name: string = "fake name";
-titulos: string = "fake name";
-mision: string = "fake name";
-foto: string = "fake name";
+  constructor (private headerService: HeaderService) {}
+
+  name: string = "fake name";
+  titulos: string = "fake name";
+  mision: string = "fake name";
+  foto: string = "fake name";
+
+  ngOnInit(): void {
+    this.headerService.getHeader()
+    .subscribe((data: any) => {
+      console.log(data);
+      //alert(data);
+      this.name = data.name:
+      this.titulos = data.titulos;
+      this.mision = data.mision;
+      this.foto = data.foto;
+    });
+  }
 
 }
